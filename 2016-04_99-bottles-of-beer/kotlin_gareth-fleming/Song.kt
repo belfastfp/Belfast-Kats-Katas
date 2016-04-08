d8e7cc0f1d7b7c63f92040769b6e2d78798038cf
Gareth Fleming <gareth.fleming@instil.co>
2016-04-08 14:40:30 +0100

    no point in returning the last line from a function. just made it a value.

--- 2016-04_99-bottles-of-beer/kotlin_gareth-fleming/Song.kt
+++ 2016-04_99-bottles-of-beer/kotlin_gareth-fleming/Song.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    (100 downTo 1).forEach{
+    (100 downTo 1).forEach {
         sing(lineFor(it))
     }
 }
@@ -7,7 +7,7 @@ fun main(args: Array<String>) {
 private fun sing(line: String) = println(line) // totally unnecessary, but makes the code read a little better
 
 private fun lineFor(numberOfBottles: Int): String =
-    if (numberOfBottles == 1) { "${bottleLine(1)}\n${noBottlesLine()}" }
+    if (numberOfBottles == 1) { "${bottleLine(1)}\n${noBottlesLine}" }
     else { "${bottleLine(numberOfBottles)}\n${oneLessBottleLine(numberOfBottles - 1)}" }
 
 private fun correctWordFor(numberOfBottles: Int): String = 
@@ -19,5 +19,5 @@ private fun bottleLine(numberOf: Int) =
 private fun oneLessBottleLine(numberOf: Int) =
     "Take one down, pass it around. ${numberOf} ${correctWordFor(numberOf)} of beer on the wall."
 
-private fun noBottlesLine() =
+private val noBottlesLine =
     "Take one down, pass it around. No more bottles of beer on the wall."
