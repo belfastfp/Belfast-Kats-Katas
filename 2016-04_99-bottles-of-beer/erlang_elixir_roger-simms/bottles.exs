cf86aa632b3d1c5f2ba8c61c54061fe8cfbc75b2
Roger Simms <roger.simms@rehabstudio.com>
2016-04-08 13:03:05 +0100

    Forgetting the basics, and the refactor fallout

--- 2016-04_99-bottles-of-beer/erlang_elixir_roger-simms/bottles.exs
+++ 2016-04_99-bottles-of-beer/erlang_elixir_roger-simms/bottles.exs
@@ -1,30 +1,21 @@
 defmodule Bottles do
-  def run, do: run(99)
+  def start, do: run(99)
 
+  defp run(-1), do: :ok
   defp run(num) do
     line1(num)
     line2(num-1)
-
-    if num > 0, do: run(num-1)
+    run(num-1)
   end
 
-  defp count_bottles(num) do
-    case num do
-      0 -> "no"
-      _ -> Integer.to_string(num)
-    end
-  end
+  defp count_bottles(0), do: "no"
+  defp count_bottles(num), do: Integer.to_string(num)
 
   defp pluralise(num), do: if num != 1, do: "s"
 
   defp line1(num), do: IO.puts "#{String.capitalize(count_bottles(num))} bottle#{pluralise(num)} of beer on the wall, #{count_bottles(num)} bottle#{pluralise(num)} of beer."
-
-  defp line2(num) do
-    case num do
-      -1 -> IO.puts "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-      _ -> IO.puts "Take one down and pass it around, #{count_bottles(num)} bottle#{pluralise(num)} of beer on the wall.\n"
-    end
-  end
+  defp line2(-1), do: IO.puts "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+  defp line2(num), do: IO.puts "Take one down and pass it around, #{count_bottles(num)} bottle#{pluralise(num)} of beer on the wall.\n"
 end
 
-# Bottles.run()
+# Bottles.start()
